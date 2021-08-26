ca5bf495e046c041a8e3407eb8790861e3e9b4ed
JohnH <john-gustav@hotmail.com>
2021-08-26 12:49:24 +0200

    Update 01 26/08/2021

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -18,9 +18,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "d2232186dd234941a5b3d89dcd87278a",
-"/": "d2232186dd234941a5b3d89dcd87278a",
-"main.dart.js": "58ab94eefd58e59b74246010414cf501",
+"index.html": "ec6d0494029dc249a7759b711e91ffd2",
+"/": "ec6d0494029dc249a7759b711e91ffd2",
+"main.dart.js": "10a22d7c80e8c8579d8b2d7ccf060da7",
 "manifest.json": "180b27026949d8b8311998c6590a4604",
 "version.json": "97240ee8860a14cf6b6ded0a82c89263"
 };
